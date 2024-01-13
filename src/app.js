3e39f79c8107031007916c039936a2ee2b820778
Aditya Shah <adityashah247@gmail.com>
2024-01-13 16:36:49 +0530

    created header and footer using handel bars

--- src/app.js
+++ src/app.js
@@ -1,28 +1,40 @@
 const path=require('path')
 const express = require('express')
- 
+const hbs=require('hbs')
 const app = express()
 
+// Define path or express config
+const publicDirectoryPath= path.join(__dirname,'../public')
+const viewsPath=path.join(__dirname,'templates/views')
+const partialsPath=path.join(__dirname,'templates/partials')
+
+// setup handelbars engie and views location
 app.set('view engine' , 'hbs')
-app.use(express.static(path.join(__dirname,'../public')))
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 
 app.get('',(req,res)=>{
     res.render('index',{
         title:'Weather!!',
-        name:'shah'
+        name:'Aditya shah'
     })
 })
 
 app.get('/about',(req,res)=>{
     res.render('about',{
         title:'About me',
-        name:'Shah'
+        name:'Aditya Shah'
     })
 })
 
 app.get('/help',(req,res)=>{
     res.render('help',{
+        title:'Help',
+        name:'Aditya Shah',
         help_message:' this is the help section'
     })
 })
