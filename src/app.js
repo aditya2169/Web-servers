c906cc3b4868709cf011d61be29a1531f71797b5
Aditya Shah <adityashah247@gmail.com>
2024-01-13 12:40:05 +0530

    used handelbars to help us render dynamic content

--- src/app.js
+++ src/app.js
@@ -3,7 +3,29 @@ const express = require('express')
  
 const app = express()
 
+app.set('view engine' , 'hbs')
 app.use(express.static(path.join(__dirname,'../public')))
+
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather!!',
+        name:'shah'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        name:'Shah'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        help_message:' this is the help section'
+    })
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
